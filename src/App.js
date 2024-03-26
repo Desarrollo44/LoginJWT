6941ac975b2c05407b650f1535c20b1fb4d5fd6e
Desarrollo44 <david.avendano@ingeal.com>
2024-03-26 10:06:00 -0500

    login y app en avance

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter,Route,Switch } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from './Auth/privateRoute';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute exact path="/login" component={Login}/>
+        <Route exact path='/register' />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,41 @@
 import { Link } from "react-router-dom";
 import { RouteComponentProps } from "react-router";
-import { React,FC } from "react";
+import { React, FC } from "react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
+import { type } from "@testing-library/user-event/dist/type";
+
+type SomeComponentsProps = RouteComponentProps;
+const Login: FC<SomeComponentsProps> = ({ history }): JSX.Element => {
+    const {
+        register,
+        handleSumbit,
+        formState: { errors },
+    } = useForm();
+
+    const login = (data) => {
+        let params = {
+            alias: data.alias,
+            password: data.password
+        };
+        axios
+            .post('/api/accountlogin', params)
+            .then(function (response) {
+                if (response.data.success = fasle) {
+                    console.log(response.data.error)
+                } else {
+                    localStorage.setItem("auth", response.data.token);
+                    setTimeout(() => {
+                        history.push("/");
+                    }, 3000);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+              });
+    };
+    return(<>
+    a
+    </>);
+}
+export default login;
